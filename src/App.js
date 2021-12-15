9954521f643e83d2a510c675293c9bd41bbb1f12
Fernnado <fcnogueirarp@gmail.com>
2021-12-15 10:04:20 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import '../src/App.css';
+import FeaturedMovie from './components/FeaturedMovie.css';
+import Header from './components/Header';
 
-function App() {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegando o Featured]
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(
+        Math.random() * originals[0].items.results.length - 1
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header />
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
-}
+};
 
-export default App;
+//parei em uma hora e 45 minutos
---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="black">
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+            alt="Netflix"
+          />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="foto_usuario"
+          />
+        </a>
+      </div>
+    </header>
+  );
+}
